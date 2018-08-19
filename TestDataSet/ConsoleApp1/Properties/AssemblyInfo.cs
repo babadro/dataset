1133aac9f5bb15d60054ce295fb3396bf6e073ec
Andrey Petrovich <baba_dro@mail.ru>
2018-08-19 08:26:11 +0300

    update to 4.7

--- TestDataSet/ConsoleApp1/Properties/AssemblyInfo.cs
+++ TestDataSet/ConsoleApp1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dataset")]
+[assembly: AssemblyTitle("ConsoleApp1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Dataset")]
+[assembly: AssemblyProduct("ConsoleApp1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e459c497-c331-4704-996c-6f24d5c2280b")]
+[assembly: Guid("5cd21d32-b58b-485e-b019-6961473e2cd0")]
 
 // Version information for an assembly consists of the following four values:
 //
--- TestDataSet/TestDataSet/MyDataSet.cs
+++ TestDataSet/TestDataSet/MyDataSet.cs
@@ -4,22 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dataset
+namespace TestDataSet
 {
     public class MyDataSet
     {
         private List<Person> _persons;
-        private Dictionary<Tuple<string, int>, double> _average;
+        private Dictionary<(string, int), double> _average;
 
         public MyDataSet(List<Person> persons)
         {
             _persons = persons;
-            _average = new Dictionary<Tuple<string, int>, double>();
+            _average = new Dictionary<(string, int), double>();
         }
 
         public double GetAverageGrossSalary(string country, int age)
         {
-            if (_average.TryGetValue())
+            if (_average.TryGetValue(new Tuple<string, int>(country, age), out double value)
             return _persons
                 .Where(p => p.Country == country && p.Age == age)
                 .Average(p => p.GrossSalary);
